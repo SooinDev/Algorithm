103821bbb9837b7b85e0a3bef5eb1927b8ecae00
sooin <144668613+sooin-kim@users.noreply.github.com>
2024-11-21 10:28:48 +0900

    [level 0] Title: rny_string, Time: 0.28 ms, Memory: 77.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+
+        answer = rny_string.replace("m", "rn");
+
+        System.out.println(answer);
+
+        return answer;
+    }
+}
