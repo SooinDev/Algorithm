577eeb74d2062cc392df60ced2363035ca2bce35
sooin <144668613+sooin-kim@users.noreply.github.com>
2024-11-21 10:29:38 +0900

    [level 0] Title: 문자열로 변환, Time: 0.26 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+
+        answer = Integer.toString(n);
+        System.out.println(answer);
+
+        return answer;
+    }
+}
