549b17ea81868eb622677905c8783006a1349a13
sooin <alwayswithsound@gmail.com>
2025-04-18 00:30:59 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 92 ms, Memory: 12788 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void solution() {
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        System.out.println(text.length());
+    }
+
+    public static void main(String[] args) {
+        solution();
+    }
+}
