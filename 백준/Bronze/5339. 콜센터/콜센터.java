eb86d8efab79cbb80aa36841215a3399727a33c5
sooin <alwayswithsound@gmail.com>
2025-06-08 11:42:13 +0900

    [Bronze V] Title: 콜센터, Time: 60 ms, Memory: 11432 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("     /~\\");
+        System.out.println("    ( oo|");
+        System.out.println("    _\\=/_");
+        System.out.println("   /  _  \\");
+        System.out.println("  //|/.\\|\\\\");
+        System.out.println(" ||  \\ /  ||");
+        System.out.println("============");
+        System.out.println("|          |");
+        System.out.println("|          |");
+        System.out.println("|          |");
+    }
+}
