641376491145ddd6fe3bb71a0ac8dc91aab6f546
sooin <alwayswithsound@gmail.com>
2025-06-08 11:40:47 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 60 ms, Memory: 11220 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
