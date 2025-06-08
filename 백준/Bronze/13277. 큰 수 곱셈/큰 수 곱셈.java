9ede8b0b6fe420aed19274d233edd90ed1fd54e4
sooin <alwayswithsound@gmail.com>
2025-06-08 16:34:13 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 5644 ms, Memory: 369412 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        BigInteger result = A.multiply(B);
+        bw.write(result.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
