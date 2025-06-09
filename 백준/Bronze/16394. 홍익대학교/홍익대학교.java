b10dec6b3df49e35f0f179f2b5838a704bb2dfc0
sooin <alwayswithsound@gmail.com>
2025-06-10 02:08:06 +0900

    [Bronze V] Title: 홍익대학교, Time: 92 ms, Memory: 12848 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int var = n - 1946;
+
+        System.out.println(var);
+    }
+}
