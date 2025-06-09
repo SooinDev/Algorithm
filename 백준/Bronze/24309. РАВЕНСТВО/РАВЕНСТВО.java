be4019deb8a7feebca16a34790e924c0e45f03a3
sooin <alwayswithsound@gmail.com>
2025-06-10 02:28:11 +0900

    [Bronze V] Title: РАВЕНСТВО, Time: 68 ms, Memory: 11808 KB -BaekjoonHub

---
+++ 백준/Bronze/24309. РАВЕНСТВО/РАВЕНСТВО.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        BigInteger c = new BigInteger(br.readLine());
+
+        BigInteger result = b.subtract(c).divide(a);
+
+        bw.write(result.toString() + "\n");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
