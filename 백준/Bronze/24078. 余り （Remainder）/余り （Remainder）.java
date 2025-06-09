b2550244d971b78da9ce57d7f89a68674764c62e
sooin <alwayswithsound@gmail.com>
2025-06-10 02:10:41 +0900

    [Bronze V] Title: 余り (Remainder), Time: 68 ms, Memory: 11584 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+        int result = X % 21;
+
+        bw.write(String.valueOf(result));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
