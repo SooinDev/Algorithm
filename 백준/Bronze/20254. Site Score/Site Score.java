a347563017e515a62672c389ced81fbe5edb93ce
sooin <alwayswithsound@gmail.com>
2025-06-13 14:36:38 +0900

    [Bronze V] Title: Site Score, Time: 64 ms, Memory: 11608 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int ur = Integer.parseInt(st.nextToken());
+        int tr = Integer.parseInt(st.nextToken());
+        int uo = Integer.parseInt(st.nextToken());
+        int to = Integer.parseInt(st.nextToken());
+        int result = ur * 56 + tr * 24 + uo * 14 + to * 6;
+        bw.write(result + "");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
