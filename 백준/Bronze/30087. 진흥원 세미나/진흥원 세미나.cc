effe1821aff269152667ff68d54a085ce7ff545a
sooin <alwayswithsound@gmail.com>
2025-07-28 21:06:33 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        string s = "";
+        cin >> s;
+
+        if (s == "Algorithm")
+            cout << "204" << "\n";
+        else if (s == "DataAnalysis")
+            cout << "207" << "\n";
+        else if (s == "ArtificialIntelligence")
+            cout << "302" << "\n";
+        else if (s == "CyberSecurity")
+            cout << "B101" << "\n";
+        else if (s == "Network")
+            cout << "303" << "\n";
+        else if (s == "Startup")
+            cout << "501" << "\n";
+        else if (s == "TestStrategy")
+            cout << "105" << "\n";
+    }
+
+    return 0;
+}
