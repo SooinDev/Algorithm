38e354cac619b10006581bd258dd4cada50db6eb
sooin <alwayswithsound@gmail.com>
2026-01-12 02:58:44 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 64 ms, Memory: 11672 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+
+        arr[0] = Integer.parseInt(st.nextToken());
+        arr[1] = Integer.parseInt(st.nextToken());
+        arr[2] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+        int result = arr[1] + arr[2];
+
+        bw.write(result + "");
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
