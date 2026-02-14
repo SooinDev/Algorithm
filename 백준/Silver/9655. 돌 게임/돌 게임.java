58e4f0b11dff0220d91de96523ddec2a02966b37
sooin <alwayswithsound@gmail.com>
2026-02-14 12:36:05 +0900

    [Silver V] Title: 돌 게임, Time: 68 ms, Memory: 11588 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n % 2 == 1)
+            bw.write("SK");
+        else
+            bw.write("CY");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
