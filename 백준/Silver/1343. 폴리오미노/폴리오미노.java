cd91dab0912a2febd2cfd942a0c66b990376a7fd
sooin <alwayswithsound@gmail.com>
2026-02-15 14:10:19 +0900

    [Silver V] Title: 폴리오미노, Time: 60 ms, Memory: 11724 KB -BaekjoonHub

---
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String board = br.readLine();
+
+        board = board.replace("XXXX", "AAAA");
+        board = board.replace("XX", "BB");
+
+        if (board.contains("X")) {
+            bw.write("-1");
+        } else {
+            bw.write(board);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
