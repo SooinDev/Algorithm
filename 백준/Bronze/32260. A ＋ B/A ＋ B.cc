ca72e74085561a69a718f0c37c4301d61a1081fb
sooin <alwayswithsound@gmail.com>
2026-02-19 23:31:42 +0900

    [Bronze V] Title: A + B, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/32260. A ＋ B/A ＋ B.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// int main() {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(NULL);
+//
+//
+//
+//     return 0;
+// }
+
+int sum(int a, int b) {
+    return a + b;
+}
