4b67b29498690b07761f16b3f85912ea14de504e
sooin <alwayswithsound@gmail.com>
2026-02-21 00:21:05 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 68 ms, Memory: 11628 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String text;
+        while ((text = br.readLine()) != null) {
+            sb.append(text).append("\n");
+        }
+
+        System.out.print(sb);
+        br.close();
+    }
+}
