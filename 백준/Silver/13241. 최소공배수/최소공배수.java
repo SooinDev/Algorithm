bfaef42a0c53e65daa10c710145bace011fe4ade
sooin <alwayswithsound@gmail.com>
2026-03-04 01:20:38 +0900

    [Silver V] Title: 최소공배수, Time: 68 ms, Memory: 11524 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+
+        long result = (a * b) / gcd(a, b);
+
+        sb.append(result);
+
+        System.out.println(sb);
+    }
+}
