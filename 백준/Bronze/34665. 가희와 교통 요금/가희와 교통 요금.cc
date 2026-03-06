57a5499a555cbac2fb0e701c129afd2a74be3a76
sooin <alwayswithsound@gmail.com>
2026-03-07 02:01:40 +0900

    [Bronze V] Title: 가희와 교통 요금, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34665. 가희와 교통 요금/가희와 교통 요금.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  string a;
+  cin >> a;
+
+  string b;
+  cin >> b;
+
+  if (a == b) {
+    cout << 0 << '\n';
+  } else {
+    cout << 1550 << '\n';
+  }
+
+  return 0;
+}
