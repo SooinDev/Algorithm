d3a23c735b36951b0a5fdb800811c163dfcadafe
sooin <alwayswithsound@gmail.com>
2026-03-26 23:49:38 +0900

    [Bronze V] Title: 덕후, Time: 64 ms, Memory: 11528 KB -BaekjoonHub

---
+++ 백준/Bronze/34921. 덕후/덕후.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+        int result = 10 + 2 * (25 - a + t);
+        
+        if (result < 0)
+            result = 0;
+        
+        sb.append(result);
+
+        System.out.print(sb);
+    }
+
+}
